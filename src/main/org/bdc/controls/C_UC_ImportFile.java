d8abb2dbc455c5d05d75d86b3e37113a9e247c33
fraottaviano <fr.ottaviano@gmail.com>
2017-09-13 22:07:39 +0200

    FRANCESCO 22.07

--- src/main/org/bdc/controls/C_UC_ImportFile.java
+++ src/main/org/bdc/controls/C_UC_ImportFile.java
@@ -1,13 +1,10 @@
 package main.org.bdc.controls;
 
-import main.org.bdc.controls.gestisciUtenti.C_UC_Login;
 import main.org.bdc.service.parser.CSVFactory;
 import main.org.bdc.service.parser.monitor.exceptions.FailedInsertException;
 import main.org.bdc.service.parser.monitor.exceptions.FailedReadException;
-import main.org.bdc.view.JFrameLogin;
 import main.org.bdc.view.JFrameMain;
 
-import javax.security.auth.login.LoginException;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
@@ -19,23 +16,70 @@ import java.awt.event.ActionListener;
  */
 public class C_UC_ImportFile extends Component {
 
-    JFrameMain view;
+    private static C_UC_ImportFile instance = null;
+
+    public synchronized static C_UC_ImportFile getInstance() {
+        if (instance == null)
+            instance = new C_UC_ImportFile();
+        return instance;
+    }
 
     public C_UC_ImportFile() {
-        EventQueue.invokeLater(new Runnable() {
+    }
 
-            @Override
-            public void run() {
+    public void importFile(){
+
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "CSV Format", "csv");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(C_UC_ImportFile.this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+            System.out.println("You chose to open this file: " +
+                    chooser.getSelectedFile().getName());
+
+            if (chooser.getSelectedFile().getName() == "higal.csv"){
                 try {
-
-                } catch (Exception e) {
+                    CSVFactory.translateFile1(chooser.getSelectedFile().getAbsolutePath());
+                } catch (FailedInsertException e) {
+                    e.printStackTrace();
+                } catch (FailedReadException e) {
                     e.printStackTrace();
                 }
             }
-        });
+            if (chooser.getSelectedFile().getName() == "higal_additionalinfo.csv"){
+                try {
+                    CSVFactory.translateFile2(chooser.getSelectedFile().getAbsolutePath());
+                } catch (FailedInsertException e) {
+                    e.printStackTrace();
+                } catch (FailedReadException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (chooser.getSelectedFile().getName() == "r08.csv"){
+                try {
+                    CSVFactory.translateFile3(chooser.getSelectedFile().getAbsolutePath());
+                } catch (FailedInsertException e) {
+                    e.printStackTrace();
+                } catch (FailedReadException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (chooser.getSelectedFile().getName() == "mips.csv"){
+                try {
+                    CSVFactory.translateFile4(chooser.getSelectedFile().getAbsolutePath());
+                } catch (FailedInsertException e) {
+                    e.printStackTrace();
+                } catch (FailedReadException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
     }
 
-    private class MntmImportActionListener implements ActionListener {
+ /*   private class MntmImportActionListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -49,14 +93,14 @@ public class C_UC_ImportFile extends Component {
                     System.out.println("You chose to open this file: " +
                             chooser.getSelectedFile().getName());
                 }
-                /*CSVFactory.translateFile1("/Users/Francesco/Desktop/higal.csv");
+                CSVFactory.translateFile1("/Users/Francesco/Desktop/higal.csv");
             } catch (FailedInsertException e1) {
                 e1.printStackTrace();
             } catch (FailedReadException e1) {
                 e1.printStackTrace();
-            }*/
+            }
         }
 
-    }
+    }*/
 
 }
