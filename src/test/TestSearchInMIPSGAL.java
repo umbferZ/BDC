39761708fb11cad7109765ece8e64c28e6988d0e
fraottaviano <fr.ottaviano@gmail.com>
2017-09-17 16:10:59 +0200

    Test Completi: Login Inserisci Utente Inserisci Satellite Inserisci Strumento Ricerca Clump per ID Ricerca in una regione (Cerchio o Quadrato/Sorgente o Clump) Ricerca Sorgente in MIPSGAL

---
+++ src/test/TestSearchInMIPSGAL.java
@@ -0,0 +1,52 @@
+package test;
+
+import main.org.bdc.controls.C_UC_SearchSource;
+import main.org.bdc.model.galaxy.Map;
+import main.org.bdc.model.galaxy.Position;
+import main.org.bdc.model.galaxy.Source;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by Sasha on 17/09/17.
+ */
+@RunWith(value = Parameterized.class)
+public class TestSearchInMIPSGAL {
+
+    private Source source;
+
+    public TestSearchInMIPSGAL(Source source) {
+        this.source = source;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Source> data() {
+
+        Source source1 = new Source();
+        source1.setMap(new Map("MIPSGAL-GAL"));
+
+        Source source2 = new Source();
+        source2.setMap(new Map("GLIMPSE"));
+
+        return Arrays.asList(source1, source2);
+    }
+
+    @Test
+    public void test() {
+
+        String[] strings = null;
+
+        try {
+            strings = C_UC_SearchSource.getInstance().searchSourceInMipsGal(178110, 250);
+        } catch (Exception e) {
+            Assert.assertTrue(false);
+        }
+        Assert.assertNotNull("Error", strings);
+
+    }
+}
